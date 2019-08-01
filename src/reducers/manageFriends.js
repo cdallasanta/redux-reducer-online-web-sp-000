d85c1e5eb406d71a1cab9ae48fcb66d23558657e
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-08-01 19:38:29 +0000

    Automatically backed up by Learn

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -3,7 +3,7 @@ export function manageFriends(state, action){
     case "ADD_FRIEND":
       return {friends: [...state.friends, action.friend]};
     case "REMOVE_FRIEND":
-      let filtered = state.friends.filter(friend => friend != action.friend)
+      let filtered = state.friends.filter(friend => friend.id != action.friend)
       return {friends: filtered}
     default:
       return state;
